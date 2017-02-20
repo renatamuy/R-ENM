4bb24965ccdf5b70d4881b194e5a527b3dc92851
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-02-20 08:45:15 -0300

    Update 02_script_selecting_numbers_ocorrrencias_10.R

--- scripts/02_script_selecting_numbers_ocorrrencias_10.R
+++ scripts/02_script_selecting_numbers_ocorrrencias_10.R
@@ -1,16 +1,16 @@
 ### script selecionar e exportar ocorrencias com certo numero de frequencia ###
 
-# Maurício Humberto Vancine - mauricio.vancine@gmail.com
+# MaurÃ­cio Humberto Vancine - mauricio.vancine@gmail.com
 
 ###--------------------------------------------------------------------###
 
 # importar dados
 
 # diretorio
-setwd("E:/__data_2016/Thadeu/modelos_borboletas/modelos/ocorrencias")
+setwd("")
 
 # dados
-da <- read.table("borboletas.txt", h = T, sep = "\t")
+da <- read.table(".txt", h = T, sep = "\t")
 da
 
 head(da)
@@ -19,10 +19,10 @@ dim(da)
 ###--------------------------------------------------------------------###
 
 # frequencias
-ocorr <- data.frame(table(da$Sp))
+ocorr <- data.frame(table(da$sp))
 head(ocorr)
 dim(ocorr)
-colnames(ocorr) <- c("Sp", "f")
+colnames(ocorr) <- c("sp", "f")
 head(ocorr)
 
 hist(ocorr$f, col = "gray", xlim = c(0, 100), ylim = c(0, 150))
@@ -48,14 +48,18 @@ da.10
 
 # conferir
 dim(da.10)
-length(unique(da.10$Sp))
+length(unique(da.10$s
+              
+              
+              
+              p))
 
 ###--------------------------------------------------------------------###
 
 # exportar as tabelas
-write.table(da.10, "ocorrencias_10.xls", sep = "\t", quote = F, row.names = F)
+write.table(da.10, ".xls", sep = "\t", quote = F, row.names = F)
 
-write.table(da.10[, -4], "ocorrencias_10.txt", sep = "\t", quote = F, row.names = F)
+write.table(da.10[, -4], ".txt", sep = "\t", quote = F, row.names = F)
 
 ###--------------------------------------------------------------------###
 
