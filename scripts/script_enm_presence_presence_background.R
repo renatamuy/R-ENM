915a7f33e7134dc1c4962de1ebcf5b2fd5793a70
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-06-06 11:31:23 -0300

    scripts

--- scripts/script_enm_presence_presence_background.R
+++ scripts/script_enm_presence_presence_background.R
@@ -3,7 +3,7 @@
 # Maurício Humberto Vancine - mauricio.vancine@gmail.com
 # 05/06/2017
 
-###-----------------------------------------------------------------------------------------###
+###---------------------------------------------------------------------------###
 
 # 1. clear memory and load packages 
 # clear workspace and increase memory
@@ -28,19 +28,17 @@ library(vegan)
 # verify packages
 search()
 
-###-----------------------------------------------------------------------------------------###
+###---------------------------------------------------------------------------###
 
 # 2. import data
 # directory
-setwd("D:/github/enm_r/data")
-
+setwd("D:/_github/enmR/data")
 
 # ocurrences
 po <- read.table("Bromelia_balansae.txt", h = T)
 head(po, 10)
 
 plot(po$long, po$lat, pch = 20)
-
 
 #  variables
 tif <- list.files(patt = "tif")
@@ -56,15 +54,15 @@ tif.21k <- grep("21k", tif, value = T)
 tif.21k
 
 env.stack.0k <- stack(tif.0k)
-names(env.stack.0k) <- paste00("bio", c("02", "04", "10", "16", "17"))
+names(env.stack.0k) <- paste0("bio", c("02", "04", "10", "16", "17"))
 env.stack.0k
 
 env.stack.6k <- stack(tif.6k)
-names(env.stack.6k) <- paste00("bio", c("02", "04", "10", "16", "17"))
+names(env.stack.6k) <- paste0("bio", c("02", "04", "10", "16", "17"))
 env.stack.6k
 
 env.stack.21k <- stack(tif.21k)
-names(env.stack.21k) <- paste00("bio", c("02", "04", "10", "16", "17"))
+names(env.stack.21k) <- paste0("bio", c("02", "04", "10", "16", "17"))
 env.stack.21k
 
 plot(env.stack.0k)
@@ -75,41 +73,45 @@ plot(env.stack.0k[[1]])
 points(po$long, po$lat, pch = 20)
 
 
-# extract values and coordinates for background
-values <- values(env.stack.0k)[, 1]
-head(values, 100)
-
+## extract coordinates for background
+# coordinates
 id <- 1:ncell(env.stack.0k)
-head(id, 100)
+head(id, 50)
+length(id)
 
-coord <- xyFromCell(env.stack.0k, id)
-coord
-
-plot(env.stack.0k[[1]])
-points(coord, pch = "o", cex = 1e-1)
-
-coords <- data.frame(coord, values)
-coords
-
-coords <- na.omit(coords)
-coords
-
-coords <- coords[, -3]
-head(coords)
-
-colnames(coords) <- c("long", "lat")
-head(coords, 10)
+co <- xyFromCell(env.stack.0k, id)
+head(coord, 50)
 
 plot(env.stack.0k[[1]])
-points(coords, pch = "o", cex = 1e-1)
+points(co, pch = "o", cex = 1e-1)
 
-###-----------------------------------------------------------------------------------------###
+# without NAs
+va <- values(env.stack.0k)[, 1]
+head(va, 50)
+length(va)
+
+co.va <- data.frame(co, va)
+head(co.va, 20)
+
+co.va.na <- na.omit(co.va)
+head(co.va.na, 10)
+
+cs <- co.va.na[, -3]
+head(cs, 10)
+
+colnames(cs) <- c("long", "lat")
+head(cs, 10)
+
+plot(env.stack.0k[[1]])
+points(cs, pch = "o", cex = 1e-1)
+
+###---------------------------------------------------------------------------###
 
 # verify maxent
-jar <- paste0(system.file(package = "dismo"), "/java/maxent.jar", sep = "")
+jar <- paste0(system.file(package = "dismo"), "/java/maxent.jar")
 file.exists(jar)
 
-###-----------------------------------------------------------------------------------------###
+###---------------------------------------------------------------------------###
 
 
 ### ENMs ###
@@ -117,8 +119,8 @@ file.exists(jar)
 # diretory
 setwd("..")
 getwd()
-dir.create("03_saidas_enm")
-setwd("03_saidas_enm")
+dir.create("ouput")
+setwd("ouput")
 getwd()
 
 # aogcms
@@ -127,12 +129,12 @@ AOGCM
 
 # enms
 for(i in 1:length(levels(po[, 1]))){ # for to each specie
+  # variables for evaluate
   eval.Bioclim <- NULL
   eval.Gower <- NULL
   eval.Maha <- NULL
   eval.Maxent <- NULL
   eval.SVM <- NULL
-
   eval.names <- NULL
 
   # selecting presence and absence
@@ -168,85 +170,87 @@ for(i in 1:length(levels(po[, 1]))){ # for to each specie
 	  eval.Bioclim.sp <- c(eBioclim@t[idBioclim], eBioclim@auc, (eBioclim@TPR[idBioclim] + eBioclim@TNR[idBioclim] - 1))
 	  eval.Bioclim <- rbind(eval.Bioclim, eval.Bioclim.sp)
 
-    # 2. gower
-	  # 1.1 calibration
+	  
+    ## 2. gower
+	  # 2.1 calibration
 	  Gower <- domain(train[which(train[, 1] == 1), -1])	
 
-	  # 1.2 projection
- writeRaster(predict(env.stack.0k, Gower), paste0(AOGCM, "_Gower_0k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
- writeRaster(predict(env.stack.6k, Gower), paste0(AOGCM, "_Gower_6k_", id.specie, r, ".tif", sep = ""), format = "GTiff")
- writeRaster(predict(env.stack.21k, Gower), paste0(AOGCM, "_Gower_21k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
- # 1.3 evaluation
-	eGower <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Gower)
-	idGower <- which(eGower@t == as.numeric(threshold(eGower, "spec_sens")))
-	eval.Gower.sp <- c(eGower@t[idGower], eGower@auc, (eGower@TPR[idGower]+eGower@TNR[idGower]-1))
-	eval.Gower <- rbind(eval.Gower, eval.Gower.sp)
+	  # 2.2 projection
+    writeRaster(predict(env.stack.0k, Gower), paste0(AOGCM, "_gower_0k_", id.specie, r, ".tif"), format = "GTiff") 
+    writeRaster(predict(env.stack.6k, Gower), paste0(AOGCM, "_gower_6k_", id.specie, r, ".tif"), format = "GTiff")
+    writeRaster(predict(env.stack.21k, Gower), paste0(AOGCM, "_gower_21k_", id.specie, r, ".tif"), format = "GTiff") 
+ 
+    # 2.3 evaluation
+	  eGower <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Gower)
+  	idGower <- which(eGower@t == as.numeric(threshold(eGower, "spec_sens")))
+	  eval.Gower.sp <- c(eGower@t[idGower], eGower@auc, (eGower@TPR[idGower] + eGower@TNR[idGower] - 1))
+	  eval.Gower <- rbind(eval.Gower, eval.Gower.sp)
 
-## Maha	
-	# 1.1 calibration
-	Maha <- mahal(train[which(train[, 1] == 1), -1])	
+	  
+    ## 3. mahanobis	
+	  # 3.1 calibration
+	  Maha <- mahal(train[which(train[, 1] == 1), -1])	
 	
-	# 1.2 projection
- writeRaster(predict(env.stack.0k, Maha), paste0(AOGCM, "_Maha_0k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
- writeRaster(predict(env.stack.6k, Maha), paste0(AOGCM, "_Maha_6k_", id.specie, r, ".tif", sep = ""), format = "GTiff")
- writeRaster(predict(env.stack.21k, Maha), paste0(AOGCM, "_Maha_21k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
+	  # 3.2 projection
+    writeRaster(predict(env.stack.0k, Maha), paste0(AOGCM, "_mahanobis_0k_", id.specie, r, ".tif"), format = "GTiff") 
+    writeRaster(predict(env.stack.6k, Maha), paste0(AOGCM, "_mahanobis_6k_", id.specie, r, ".tif"), format = "GTiff")
+    writeRaster(predict(env.stack.21k, Maha), paste0(AOGCM, "_mahanobis_21k_", id.specie, r, ".tif"), format = "GTiff") 
  
- # 1.3 evaluation
-	eMaha <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maha)
-	idMaha <- which(eMaha@t == as.numeric(threshold(eMaha, "spec_sens")))
-	eval.Maha.sp <- c(eMaha@t[idMaha], eMaha@auc, (eMaha@TPR[idMaha]+eMaha@TNR[idMaha]-1))
-	eval.Maha <- rbind(eval.Maha, eval.Maha.sp)
+    # 3.3 evaluation
+	  eMaha <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maha)
+	  idMaha <- which(eMaha@t == as.numeric(threshold(eMaha, "spec_sens")))
+	  eval.Maha.sp <- c(eMaha@t[idMaha], eMaha@auc, (eMaha@TPR[idMaha] + eMaha@TNR[idMaha] - 1))
+	  eval.Maha <- rbind(eval.Maha, eval.Maha.sp)
 	
 
-## Maxent	
-	# 1.1 calibration
-	Maxent <- maxent(train[, -1], train[, 1])	
+    ## 4. maxent	
+	  # 4.1 calibration
+	  Maxent <- maxent(train[, -1], train[, 1])	
 
-	# 1.2 projection
- writeRaster(predict(env.stack.0k, Maxent), paste0(AOGCM, "_Maxent_0k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
- writeRaster(predict(env.stack.6k, Maxent), paste0(AOGCM, "_Maxent_6k_", id.specie, r, ".tif", sep = ""), format = "GTiff")
- writeRaster(predict(env.stack.21k, Maxent), paste0(AOGCM, "_Maxent_21k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
+	  # 4.2 projection
+    writeRaster(predict(env.stack.0k, Maxent), paste0(AOGCM, "_maxent_0k_", id.specie, r, ".tif"), format = "GTiff") 
+    writeRaster(predict(env.stack.6k, Maxent), paste0(AOGCM, "_maxent_6k_", id.specie, r, ".tif"), format = "GTiff")
+    writeRaster(predict(env.stack.21k, Maxent), paste0(AOGCM, "_maxent_21k_", id.specie, r, ".tif"), format = "GTiff") 
  
- # 1.3 evaluation
-	eMaxent <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maxent)
-	idMaxent <- which(eMaxent@t == as.numeric(threshold(eMaxent, "spec_sens")))
-	eval.Maxent.sp <- c(eMaxent@t[idMaxent], eMaxent@auc, (eMaxent@TPR[idMaxent]+eMaxent@TNR[idMaxent]-1))
-	eval.Maxent <- rbind(eval.Maxent, eval.Maxent.sp)
+    # 4.3 evaluation
+	  eMaxent <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = Maxent)
+	  idMaxent <- which(eMaxent@t == as.numeric(threshold(eMaxent, "spec_sens")))
+	  eval.Maxent.sp <- c(eMaxent@t[idMaxent], eMaxent@auc, (eMaxent@TPR[idMaxent] + eMaxent@TNR[idMaxent] - 1))
+	  eval.Maxent <- rbind(eval.Maxent, eval.Maxent.sp)
 
 
-## SVM	
-	# 1.1 calibration
-	SVM <- ksvm(pb ~ bio02 + bio04 + bio10 + bio16 + bio17, data = train)	
+    ## 5. svm	
+	  # 5.1 calibration
+	  SVM <- ksvm(pb ~ bio02 + bio04 + bio10 + bio16 + bio17, data = train)	
 
-	# 1.2 projection
- writeRaster(predict(env.stack.0k, SVM), paste0(AOGCM, "_SVM_0k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
- writeRaster(predict(env.stack.6k, SVM), paste0(AOGCM, "_SVM_6k_", id.specie, r, ".tif", sep = ""), format = "GTiff")
- writeRaster(predict(env.stack.21k, SVM), paste0(AOGCM, "_SVM_21k_", id.specie, r, ".tif", sep = ""), format = "GTiff") 
+	  # 5.2 projection
+    writeRaster(predict(env.stack.0k, SVM), paste0(AOGCM, "_svm_0k_", id.specie, r, ".tif"), format = "GTiff") 
+    writeRaster(predict(env.stack.6k, SVM), paste0(AOGCM, "_svm_6k_", id.specie, r, ".tif"), format = "GTiff")
+    writeRaster(predict(env.stack.21k, SVM), paste0(AOGCM, "_svm_21k_", id.specie, r, ".tif"), format = "GTiff") 
  
- # 1.3 evaluation
-	eSVM <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = SVM)
-	idSVM <- which(eSVM@t == as.numeric(threshold(eSVM, "spec_sens")))
-	eval.SVM.sp <- c(eSVM@t[idSVM], eSVM@auc, (eSVM@TPR[idSVM]+eSVM@TNR[idSVM]-1))
-	eval.SVM <- rbind(eval.SVM, eval.SVM.sp)
+    # 5.3 evaluation
+	  eSVM <- evaluate(p = test[test[, 1] == 1, -1], a = test[test[, 1] == 0, -1], model = SVM)
+	  idSVM <- which(eSVM@t == as.numeric(threshold(eSVM, "spec_sens")))
+	  eval.SVM.sp <- c(eSVM@t[idSVM], eSVM@auc, (eSVM@TPR[idSVM] + eSVM@TNR[idSVM] - 1))
+	  eval.SVM <- rbind(eval.SVM, eval.SVM.sp)
 
+	  eval.names <- c(eval.names, paste0(id.specie, r))	
+	  
+  } # ends for "r"
 
-	eval.names <- c(eval.names, paste0(id.specie, r, sep = ""))		
-} # ends for "r"
+  dimnames(eval.Bioclim) <- list(eval.names, c("thrs", "AUC", "TSS"))
+  dimnames(eval.Gower) <- list(eval.names, c("thrs", "AUC", "TSS"))  
+  dimnames(eval.Maha) <- list(eval.names, c("thrs", "AUC", "TSS"))  
+  dimnames(eval.Maxent) <- list(eval.names, c("thrs", "AUC", "TSS"))
+  dimnames(eval.SVM) <- list(eval.names, c("thrs", "AUC", "TSS"))
 
-dimnames(eval.Bioclim) <- list(eval.names, c("thrs", "AUC", "TSS"))
-dimnames(eval.Gower) <- list(eval.names, c("thrs", "AUC", "TSS"))
-dimnames(eval.Maha) <- list(eval.names, c("thrs", "AUC", "TSS"))
-dimnames(eval.Maxent) <- list(eval.names, c("thrs", "AUC", "TSS"))
-dimnames(eval.SVM) <- list(eval.names, c("thrs", "AUC", "TSS"))
-
-
-write.table(eval.Bioclim, paste0("zEval_", AOGCM, "_Bioclim_", id.specie, ".txt", sep = ""))
-write.table(eval.Gower, paste0("zEval_", AOGCM, "_Gower_", id.specie, ".txt", sep = ""))
-write.table(eval.Maha, paste0("zEval_", AOGCM, "_Maha_", id.specie, ".txt", sep = ""))
-write.table(eval.Maxent, paste0("zEval_", AOGCM, "_Maxent_", id.specie, ".txt", sep = ""))
-write.table(eval.SVM, paste0("zEval_", AOGCM, "_SVM_", id.specie, ".txt", sep = ""))
+  write.table(eval.Bioclim, paste0("zEval_", AOGCM, "_bioclim_", id.specie, ".txt"))
+  write.table(eval.Gower, paste0("zEval_", AOGCM, "_gower_", id.specie, ".txt"))
+  write.table(eval.Maha, paste0("zEval_", AOGCM, "_maha_", id.specie, ".txt"))
+  write.table(eval.Maxent, paste0("zEval_", AOGCM, "_maxent_", id.specie, ".txt"))
+  write.table(eval.SVM, paste0("zEval_", AOGCM, "_svm_", id.specie, ".txt"))
 
 } # ends for"i"
 
-###-----------------------------------------------------------------------------------------###
+###----------------------------------------------------------------------------###
 
