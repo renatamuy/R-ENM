9b30e931bfeabe9c508d8b5c09f293d823b5142c
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-06-28 22:25:25 -0300

    script

--- scripts/script_ecoland_general.R
+++ scripts/script_ecoland_general.R
@@ -13,7 +13,7 @@ memory.limit(size = 1.75e13)
 
 # packages
 if(!require("pacman")) install.packages("pacman")
-pacman::p_load(raster, rgdal, dismo, data.table, colorRamps)
+pacman::p_load(raster, rgdal, data.table, colorRamps, ggplot2, hexbin)
 
 # verify packages
 search()
@@ -45,41 +45,143 @@ plot(br, add = T)
 ## classificacao em 4 quadrantes
 
 # values
-da <- data.table(id = 1:ncell(re), cl = re[], la = rl[], col = "NA")
+da <- data.table(id = 1:ncell(re.b), cl = re.b[], la = rl.b[], col = "NA", val = 0)
 da
 
 # classificacao 
 for(i in 1:nrow(da)){
-  if(da[i, 2] <= .5 & da[i, 3] <= .5){
-    da[i, 4] <- "blue"} 
-  else if(da[i, 2] <= .5 & da[i, 3] > .5){  
-    da[i, 4] <- "green"} 
-  else if(da[i, 2] > .5 & da[i, 3] <= .5){  
-    da[i, 4] <- "orange"} 
-  else if(da[i, 2] > .5 & da[i, 3] > .5){  
-    da[i, 4] <- "red"}}
+  if(is.na(da$cl[i])){
+    
+    da[i, 5] <- NA
+    
+    } else{
+    if(da[i, 2] <= .5 & da[i, 3] <= .5){
+      da[i, 4] <- "blue"
+      da[i, 5] <- 0} 
+    else if(da[i, 2] <= .5 & da[i, 3] > .5){  
+      da[i, 4] <- "green"
+      da[i, 5] <- .25} 
+    else if(da[i, 2] > .5 & da[i, 3] <= .5){  
+      da[i, 4] <- "orange"
+      da[i, 5] <- .75} 
+    else if(da[i, 2] > .5 & da[i, 3] > .5){  
+      da[i, 4] <- "red"
+      da[i, 5] <- 1}}}
 
 da
+
+# plot normal
+par(mar = c(5, 5, 2, 2))
+plot(da$cl, da$la, type = "n",
+     xlim = c(0, 1),
+     ylim = c(0, 1),
+     xlab = "Climate suitability", 
+     ylab = "Landscape suitability",
+     col.axis = 'grey30',					
+     cex.lab = 1.6,												
+     cex.axis = 1.2, 
+     las = 1)
+
+smoothScatter(da[, 2:3], nrpoints = 0, add = T, colramp = colorRampPalette(c("white", "gray")))
+
+points(da$cl, da$la, col = da$col, pch = 20, cex = .8)
+
+abline(h = .5 , v = .5, col = "gray", lty = 2)
+
+
+# map
+reco <- re.b
+reco
+
+reco[] <- da$val
+
+length(da$val)
+
+plot(reco, col = c("blue", "green", "orange", "red"))
+
+
 
 ###---------------------------------------------------------------------------###
 
 ## classificacao em 9 quadrantes
 
 # values
-da <- data.table(id = 1:ncell(re), cl = re[], la = rl[], col = "NA")
+da <- data.table(id = 1:ncell(re.b), cl = re.b[], la = rl.b[], col = "NA", val = 0)
 da
 
 # classificacao 
 for(i in 1:nrow(da)){
-  if(da[i, 2] <= .5 & da[i, 3] <= .5){
-    da[i, 4] <- "blue"} 
-  else if(da[i, 2] <= .5 & da[i, 3] > .5){  
-    da[i, 4] <- "green"} 
-  else if(da[i, 2] > .5 & da[i, 3] <= .5){  
-    da[i, 4] <- "orange"} 
-  else if(da[i, 2] > .5 & da[i, 3] > .5){  
-    da[i, 4] <- "red"}}
+  if(is.na(da$cl[i])){
+    
+    da[i, 5] <- NA
+    
+  } else{
+    if(da[i, 2] <= .25 & da[i, 3] <= .25){
+      da[i, 4] <- "blue"
+      da[i, 5] <- 0} 
+    
+    else if(da[i, 2] <= .25 & 
+            da[i, 3] > .25 & da[i, 3] <= .75){  
+      da[i, 4] <- "cyan"
+      da[i, 5] <- .25} 
+    
+    else if(da[i, 2] > .25 & da[i, 2] <= .75 &
+            da[i, 3] <= 25){  
+      da[i, 4] <- "chocolate"
+      da[i, 5] <- .25} 
+    
+    else if(da[i, 2] > .25 & da[i, 2] <= .75 &
+            da[i, 3] > .25 & da[i, 3] <= .75){  
+      da[i, 4] <- "green"
+      da[i, 5] <- .25} 
+    
+    else if(da[i, 2] > .75 & 
+            da[i, 3] <= .25){  
+      da[i, 4] <- "chocolate"
+      da[i, 5] <- .25} 
+    
+    else if(da[i, 2] <= .25 & 
+            da[i, 3] > .75){  
+      da[i, 4] <- "yellow"
+      da[i, 5] <- .75} 
+    
+    else if(da[i, 2] > .75 & da[i, 3] > .75){  
+      da[i, 4] <- "red"
+      da[i, 5] <- 1}}}
+
 da
+
+# plot
+par(mar = c(5, 5, 2, 2))
+plot(da$cl, da$la, type = "n",
+     xlim = c(0, 1),
+     ylim = c(0, 1),
+     xlab = "Climate suitability", 
+     ylab = "Landscape suitability",
+     col.axis = 'grey30',					
+     cex.lab = 1.6,												
+     cex.axis = 1.2, 
+     las = 1)
+
+smoothScatter(da[, 2:3], nrpoints = 0, add = T, colramp = colorRampPalette(c("white", "gray")))
+
+points(da$cl, da$la, col = da$col, pch = 20, cex = .8)
+
+abline(h = .25 , v = .25, col = "gray", lty = 2)
+abline(h = .75 , v = .75, col = "gray", lty = 2)
+
+
+
+# map
+reco <- re.b
+reco
+
+reco[] <- da$val
+
+length(da$val)
+
+plot(reco, col = c("blue", "green", "orange", "red"))
+
 
 ###---------------------------------------------------------------------------###
 
