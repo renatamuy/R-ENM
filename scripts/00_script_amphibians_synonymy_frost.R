2979309e9aebf89623fd390dc93826fe7191961b
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-07-28 03:03:11 -0300

    script

--- scripts/00_script_amphibians_synonymy_frost.R
+++ scripts/00_script_amphibians_synonymy_frost.R
@@ -1,6 +1,6 @@
 ### script amphibians synonymys frost ###
 
-# Mauricio Humberto Vancine - mauricio.vancine@gmail.com
+# Maurício Humberto Vancine - mauricio.vancine@gmail.com
 
 ###---------------------------------------------------------------------###
 
@@ -11,50 +11,71 @@ memory.limit(size = 1.75e13)
 
 # packages 
 if(!require("pacman")) install.packages("pacman")
-pacman::p_load(rvest, data.table)
+pacman::p_load(rvest, data.table, stringr)
 
 ###---------------------------------------------------------------------###
-###				               amphibians synonymys frost	              			###
+###				        amphibians synonymys frost	       			###
 ###---------------------------------------------------------------------###
 
+# data
+da <- data.table()
 
 # html
 for(i in seq(10, 8360, 10)){
+ 
+ # http of search
+ url <- paste0("http://research.amnh.org/vz/herpetology/amphibia/amphib/basic_search/(offset)/", i,
+        "/(query)/*")
+ 
+ # page
+ pg <- read_html(url)
+ 
+ # link
+ li <- html_nodes(pg, "a") %>%
+  html_attr("href")
+ 
+ # species
+ sp <- grep("-", grep("Amphibia", li, value = T), value = T)
+
+ 
+ for(j in sp){
   
-  # http of search
-  url <- paste0("http://research.amnh.org/vz/herpetology/amphibia/amphib/basic_search/(offset)/", i,
-                "/(query)/*")
+  # page of specie
+  pg.sp <- read_html(paste0("http://research.amnh.org", j))
   
-  # page
-  pg <- read_html(url)
+  pg.sp <- read_html("http://research.amnh.org/vz/herpetology/amphibia/Amphibia/Anura/Hyperoliidae/Hyperolius/Hyperolius-rwandae")
   
-  # link
-  li <- html_nodes(pg, "a") %>%
-    html_attr("href")
+  # names
+  na.p <- html_nodes(pg.sp, "title") %>%
+   html_text()
   
-  # species
-  sp <- grep("-", grep("Amphibia", li, value = T), value = T)
+  na.c <- sub("  Amphibian Species of the World", "", str_replace(na.p, "[|]", ""))
+  
+  na.s <- paste(str_split(na.c, boundary("word"))[[1]][1], 
+                str_split(na.c, boundary("word"))[[1]][2])
+  
+  na.a <- tolower(sub(" ", "_", na.s))
+  
+  na <- data.frame(name = na.c, name_j = na.s, name_s = na.a)
+  na
+  
+  # synonymies
+  sy <- html_node(pg.sp, "div.synonymy") %>%
+   html_nodes("d") %>%
+   html_text()
+  sy
+  
+  te <- cbind.data.frame(na, synonymes = sy)
+  
+  fwrite(te, paste0(na.a, ".txt"), sep = "\t")
+ 
+  da <- rbind(da, te)}}
+
+
+fwrite(da, "synonymes_amphibia_frost.txt", sep = "\t")
+
 
   
-  for(j in sp){
-    
-    # page of specie
-    pg.sp <- read_html(paste0("http://research.amnh.org", j))
-    
-    # names
-    na <- html_nodes(pg.sp, "h2") %>%
-      html_text() %>%
-      '['(3)
-    
-    na.c <- sub("\n            ", "", sub("\n                    ", "", na), na)
-    na.c
-   
-    # synonymies
-    sy <- html_node(pg.sp, "div.synonymy") %>%
-      html_nodes("b") %>%
-      html_text()
-    sy}}
-    
 
 ###---------------------------------------------------------------------###
 
