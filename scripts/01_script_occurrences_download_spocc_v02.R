a9395db59794226e5edc3196e8a0decc93c5f689
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-08-15 12:46:54 -0300

    script

--- scripts/01_script_occurrences_download_spocc_v02.R
+++ scripts/01_script_occurrences_download_spocc_v02.R
@@ -11,7 +11,7 @@ gc()
 memory.limit(size = 1.75e13) 
 
 ## packages
-pacman::p_load(spocc, data.table, dplyr, ggmap)
+pacman::p_load(spocc, data.table, dplyr, stringr, ggmap)
 
 # verify
 search()
@@ -21,11 +21,11 @@ search()
 ## import list of species
 setwd("E:/github/enmR/occurrence")
 
-sp <- fread("")
-sp
+# sp <- fread("")
+# sp
 
 sp <- c("Haddadus binotatus", "Macrogenioglottus alipioi", "Myersiella microps",
-        "Proceratophyrus boiei", "Vitreorana eurygnatha", "Vitreorana uranoscopa")
+        "Proceratophrys boiei", "Vitreorana eurygnatha", "Vitreorana uranoscopa")
 
 ###---------------------------------------------------------------------------###
 
@@ -36,48 +36,54 @@ ba <- c("gbif", "ebird", "ecoengine", "bison", "antweb", "vertnet",
 ###---------------------------------------------------------------------------###
 
 ## download data
-
-# empty data
-da <- data.table()
-
+# map
+af <- get_map(location = c(-60, 0, -30, -40), zoom = 5)
 
 # for
 for(i in sp){
   
-  re <- occ(query = sp, from = ba)
+  re <- occ(query = i, from = ba, limit = 5000)
+  
+  print(i)
+  
+  da <- data.table()
   
   for(j in 1:length(ba)){
     
-      if(dim(re[[j]][[2]][[1]]) == 0){print(paste0("Without data for ", ba[j]))
+    if(dim(re[[j]][[2]][[1]]) == 0){print(paste("Without data for", i, "in", ba[j]))
       
-      } else{
-        
-        da.b <- data.table(re[[j]][[2]][[1]]$name,
-                           re[[j]][[2]][[1]]$longitude,
-                           re[[j]][[2]][[1]]$latitude,
-                           re[[j]][[2]][[1]]$prov)
-        
-        da <- rbind(da, da.b, fill = T)}}
-        
-        colnames(da) <- c("sp", "longitude", "latitude", "base")
-        da$longitude <- as.numeric(as.character(da$longitude))
-        da$latitude <- as.numeric(as.character(da$latitude))
-        
-        fwrite(da, paste0("occurrence_", sub(" ", "_", i), ".txt"), 
-               sep = "\t", quote = F, row.names = F)
-        
-        # map
-        af <- get_map(location = c(-60, 0, -30, -40), zoom = 5)
-        
-        ggmap(af, extent = "panel") +
-          geom_point(data = da, aes(x = longitude, y = latitude, fill = base), 
-                     shape = 21, size = 3) +
-          theme(legend.position = c(0.9, 0.2),
-                legend.background = element_rect(color = "black", fill = "grey90", 
-                                                 size = 1, linetype = "solid")) +
-          scale_fill_discrete(name = "Bases") + 
-          ggtitle(paste0("Occurrences of ", i))
-        
-        ggsave(paste0("map_", i, ".tiff"), dpi = 600)}
+    } else{
+      
+      print(paste("Yep! There is data for", i, "in", ba[j]))
+      
+      da.b <- data.table(re[[j]][[2]][[1]]$name,
+                         re[[j]][[2]][[1]]$longitude,
+                         re[[j]][[2]][[1]]$latitude,
+                         re[[j]][[2]][[1]]$prov)
+      
+      da <- rbind(da, da.b, fill = T)}}
+  
+  colnames(da) <- c("sp", "long", "lat", "base")
+  da$long <- as.numeric(as.character(da$long))
+  da$lat <- as.numeric(as.character(da$lat))
+  
+  da.na <- na.omit(da)
+  da.na$sp <- str_to_lower(sub(" ", "_", i))
+  
+  fwrite(da.na, paste0("occurrence_spocc_", str_to_lower(sub(" ", "_", i)), ".txt"), 
+         sep = "\t", quote = F, row.names = F)
+  
+  
+  # map
+  ggmap(af, extent = "panel") +
+    geom_point(data = da, aes(x = long, y = lat, fill = base), 
+               shape = 21, size = 3) +
+    theme(legend.position = c(0.9, 0.2),
+          legend.background = element_rect(color = "black", fill = "grey90", 
+                                           size = 1, linetype = "solid")) +
+    scale_fill_discrete(name = "Bases") + 
+    ggtitle(paste0("Occurrences of ", i))
+  
+  ggsave(paste0("map_", str_to_lower(sub(" ", "_", i)), ".tiff"), dpi = 600)}
 
 ###---------------------------------------------------------------------------###
