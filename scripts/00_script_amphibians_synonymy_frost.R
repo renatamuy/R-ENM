dd78a7ac6a0eb136c387d0d789c62d0feead78a5
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-08-25 02:39:01 -0300

    script

--- scripts/00_script_amphibians_synonymy_frost.R
+++ scripts/00_script_amphibians_synonymy_frost.R
@@ -13,7 +13,7 @@ memory.limit(size = 1.75e13)
 
 # packages 
 if(!require("pacman")) install.packages("pacman")
-pacman::p_load(rvest, data.table, stringr, plyr)
+pacman::p_load(rvest, data.table, stringr, plyr, dplyr)
 
 ###---------------------------------------------------------------------###
 
@@ -85,98 +85,37 @@ for(i in seq(10, n.se, 10)){
     t() %>%
     trimws()
   
-  colnames(ta) <- ta[1, ]
-  ta <- ta[2, ]
-  ta
-  
+  ta.na <- str_to_lower(ta[1, ])
+  ta.da <- ta[2, ]
+
   # synonymies
   sy.na <- html_node(pg.sp, "div.synonymy") %>%
     html_nodes("b") %>%
     html_text()
-  sy.na
   
-  sy.re <- html_node(pg.sp, "div.synonymy") %>%
+  sy.in <- html_node(pg.sp, "div.synonymy") %>%
     html_nodes("p") %>%
-    html_nodes("a") %>%
     html_text()
-  sy.re
   
-  eng.na <- html_nodes(pg.sp, "h3") %>%
-    html_node("p") %>%
-    html_text()
-  eng.na
+  if(length(sy.na) == 0){
+    da.sy <- data.frame(synonymies = NA, reference = sy.in, 
+                        link = paste0("http://research.amnh.org", j))
+    
+  } else{
+    da.sy <- data.frame(synonymies = sy.na, reference = sy.in, 
+                      link = paste0("http://research.amnh.org", j))
+  }
   
-  inf <- in.pg[1]
+  # data
+  ta <- data.table(matrix(rep(ta.da, each = nrow(da.sy)), nrow(da.sy)))
+  colnames(ta) <- ta.na
   
-  dis <- re.in[2]
-  
+  da <- bind_rows(da, cbind(ta, da.sy))
   
   }
- 
+  }
 
-  
-  # information
-  in.pg <- html_nodes(pg.sp, "p") %>%
-    html_text()
-  in.pg
-  
-  inf <- re.in[1]
-  
-  dis <- re.in[2]
-  
-  re.li <- html_nodes(pg.sp, "a") %>%
-    html_attr("href")
-  re.li <- grep("node", re.li, value = T)
-  
-  re.pg <- read_html(paste0("http://research.amnh.org", re.li))
-  
-  re <- html_nodes(re.sp, "div#aswContent") %>%
-    html_text()
-  
-  re <- sub(" \n", "", sub("\n    Bibliography\n    ", "", re))
-  
-
-  }}
-
-
+# export
 fwrite(da, "synonymes_amphibia_frost.csv", na = "NA")
 
-
-
-
 ###---------------------------------------------------------------------###
-
-
-na.pg <- html_nodes(pg.sp, "title") %>%
-  html_text()
-
-na.de <- sub("  Amphibian Species of the World", "", str_replace(na.pg, "[|]", ""))
-
-na <- paste(str_split(na.de, boundary("word"))[[1]][1], 
-            str_split(na.de, boundary("word"))[[1]][2])
-
-na.ab <- tolower(sub(" ", "_", na))
-
-na <- data.frame(name = na, name_description = na.de, name_abbreviate = na.ab)
-
-## synonymies
-sy <- html_node(pg.sp, "div.synonymy") %>%
-  html_nodes("d") %>%
-  html_text()
-sy
-
-te <- cbind.data.frame(na, synonymes = sy)
-
-fwrite(te, paste0(na.a, ".txt"), sep = "\t")
-
-da <- rbind(da, te)
-
-da <- rbind(da, data.table(cbind(class = "Amphibia", order = or[i], 
-                                 superfamily = sup, family = NA, 
-                                 subfamily = NA, genus = sup.ge[j], 
-                                 species = sp, link = li.sp)))
-
-
-
-
-
